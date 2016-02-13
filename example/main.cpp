cc13f8bc8166e2ff23940c2bcae6881776c23164
NanoCat <shablya24@gmail.com>
2016-02-13 21:37:41 +0200

    Update example

--- example/main.cpp
+++ example/main.cpp
@@ -1,5 +1,4 @@
 // This file is usage demo for SSDE (http://github.com/notnanocat/ssde)
-#include "../ssde/ssde.hpp"
 #include "../ssde/ssde_x86.hpp"
 #include "../ssde/ssde_x64.hpp"
 #include "../ssde/ssde_arm.hpp"
@@ -11,17 +10,18 @@
 int main(int argc, const char* argv[])
 {
 	using namespace std;
+	using namespace ssde;
 
 	ios::sync_with_stdio(false);
 
 
-	const string bc_arm {"\x01\x20\x40\xe2"
+	/*const string bc_arm {"\x01\x20\x40\xe2"
 	                     "\x02\x20\x61\xe0"
 	                     "\x01\x30\xd1\xe4"
 	                     "\x00\x00\x53\xe3"
 	                     "\x02\x30\xc1\xe7"
 	                     "\xfb\xff\xff\x1a"
-	                     "\x1e\xff\x2f\xe1", 4*7};
+	                     "\x1e\xff\x2f\xe1", 4*7};*/
 
 	/*
 	for (ssde::Disasm<ssde::Inst_arm> dis {bc_arm};
@@ -41,25 +41,26 @@ int main(int argc, const char* argv[])
 		cout << "\n";
 	}
 	*/
+
 	
 #if 0
-		 const string bc_x86 {"\x55"
-			 "\x31\xd2"
-			 "\x89\xe5"
-			 "\x8b\x45\x08"
-			 "\x56"
-			 "\x8b\x75\x0c"
-			 "\x53"
-			 "\x8d\x58\xff"
-			 "\x0f\xb6\x0c\x16"
-			 "\x88\x4c\x13\x01"
-			 "\x83\xc2\x01"
-			 "\x84\xc9"
-			 "\x75\xf1"
-			 "\x5b"
-			 "\x5e"
-			 "\x5d"
-			 "\xc3"};
+	const string bc_x86 {"\x55"
+	                     "\x31\xd2"
+	                     "\x89\xe5"
+	                     "\x8b\x45\x08"
+	                     "\x56"
+	                     "\x8b\x75\x0c"
+	                     "\x53"
+	                     "\x8d\x58\xff"
+	                     "\x0f\xb6\x0c\x16"
+	                     "\x88\x4c\x13\x01"
+	                     "\x83\xc2\x01"
+	                     "\x84\xc9"
+	                     "\x75\xf1"
+	                     "\x5b"
+	                     "\x5e"
+	                     "\x5d"
+	                     "\xc3"};
 
 	for (ssde::Disasm<ssde::Inst_x86> dis {bc_x86};
 	     dis.has_next();
